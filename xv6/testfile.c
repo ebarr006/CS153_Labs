5f3bcb60baa0465d084b740403a0e0916af499ee
lgonz041 <lgonz041@ucr.edu>
2017-10-27 18:15:39 +0000

    testing wait

--- xv6/testfile.c
+++ xv6/testfile.c
@@ -1,32 +1,33 @@
 #include "types.h"
 #include "user.h"
-//#include "defs.h"
+
 int 
 main( int argc, char **argv){
 	
-	printf(1, "Test for exit, wait, and waitpid syscals ;) \n");
+	printf(1, "Test for exit, wait, and waitpid syscals ;) \n \n");
 	
 	int pid1 = fork();
+	wait(0);
 	
-	printf(1,"First pid: %d\n",pid1);
+	printf(1,"First pid: %d\n\n",pid1);
 	
 	if(pid1 == 0 ){
 		
 		wait(0);	
 		printf(1,"Parent1 with child pid: %d \n", pid1);
+		
 	}else{
-		int pid2 = fork();
+		//int pid2 = fork();
 	        
 		//waitpid(pid2, 0 , 0);
-		if(pid2 == 0){
-			wait(0);
+		//if(pid2 == 0){
+		//	wait(0);
 			printf(1,"child with pid: %d \n",pid1);
-		}else{
-			printf(1,"child2 pid: %d\n",pid2);
-		}	
+		//}else{
+		//	printf(1,"child2 pid: %d\n",pid2);
+		//}	
 	}
 	
 
-	//exit(0);
 
 }
