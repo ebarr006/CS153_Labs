b580af090af1faa5c46f3e40e07aee31df7ffca0
lgonz041 <lgonz041@ucr.edu>
2017-11-29 06:10:31 +0000

    Working with statck on top

--- Lab2/syscall.c
+++ Lab2/syscall.c
@@ -17,9 +17,9 @@
 int
 fetchint(uint addr, int *ip)
 {
-  struct proc *curproc = myproc();
+  //struct proc *curproc = myproc();
 
-  if(addr >= (KERNBASE-1) || addr+4 > (KERBASE-1))
+  if(addr >= (KERNBASE) || addr+4 > (KERNBASE))
     return -1;
   *ip = *(int*)(addr);
   return 0;
@@ -34,7 +34,7 @@ fetchstr(uint addr, char **pp)
   char *s, *ep;
   struct proc *curproc = myproc();
 
-  if(addr >=( KERNBASE -1))
+  if(addr >=( KERNBASE ))
     return -1;
   *pp = (char*)addr;
   ep = (char*)curproc->sz;
@@ -59,11 +59,11 @@ int
 argptr(int n, char **pp, int size)
 {
   int i;
-  struct proc *curproc = myproc();
+  //struct proc *curproc = myproc();
  
   if(argint(n, &i) < 0)
     return -1;
-  if(size < 0 || (uint)i >= (KERNBASE-1) || (uint)i+size > (KERNBASE-1))
+  if(size < 0 || (uint)i >= (KERNBASE) || (uint)i+size > (KERNBASE))
     return -1;
   *pp = (char*)i;
   return 0;
